c3b86fe15abfc75e87d6e0256d0d50cfdf957fc5
Jym Bocala <jym.bocala@gmail.com>
2023-05-27 23:47:53 +1000

    create useRef variables & handleSubmit function

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,15 +1,24 @@
+import { FormEvent, useRef } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 
 export function NoteForm() {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control ref={titleRef} required />
             </Form.Group>
           </Col>
           <Col>
@@ -21,11 +30,17 @@ export function NoteForm() {
         </Row>
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" rows={15} />
+          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
         </Form.Group>
         <Stack direction={"horizontal"} gap={2} className="justify-content-end">
-          <Button type="submit" variant="primary">Save</Button>
-          <Button type="button" variant="outline-secondary">Cancel</Button>
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
         </Stack>
       </Stack>
     </Form>
