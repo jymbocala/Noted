f027ec8d987c22a253d22b6c871ec23e083d4ed3
Jym Bocala <jym.bocala@gmail.com>
2023-05-29 16:51:41 +1000

    add comments

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-  // Checking if the value exists yet
   const [value, setValue] = useState<T>(() => {
+    // Checking if the value exists in local storage
     const jsonValue = localStorage.getItem(key);
+    // If the value is not found, set it to the initial value provided
     if (jsonValue == null) {
       if (typeof initialValue === "function") {
+        // If initialValue is a function, call it to get the initial value
         return (initialValue as () => T)();
       } else {
+        // Otherwise, use the provided initial value directly
         return initialValue;
       }
     } else {
+      // If the value exists, parse and return it
       return JSON.parse(jsonValue);
     }
   });
 
-  // Update everytime value changes
+  // Update local storage wheneever value changes
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
 
+  // Return the value and the setValue function as an array
   return [value, setValue] as [T, typeof setValue];
 }
