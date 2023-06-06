af74ba61210e9f5a48acce652b611f3354664b35
Jym Bocala <jym.bocala@gmail.com>
2023-06-06 22:59:43 +1000

    create Note component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useLocalStorage } from "./useLocalStorage";
 import { v4 as uuidV4 } from "uuid";
 import { NoteList } from "./NoteList";
 import { NoteLayout } from "./NoteLayout";
+import { Note } from "./Note";
 
 // Type representing a Note
 export type Note = {
@@ -79,7 +80,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={noteWithTags} />}>
-          <Route index element={<h1>Show</h1>} />
+          <Route index element={<Note/>} />
           <Route path="edit" element={<h1>Edit</h1>} />
         </Route>
         {/* Catch-all route to redirect to the home page */}
--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -107,13 +107,22 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
       className={`h-100 text-reset text-decoration-none ${styles.card}`}
     >
       <Card.Body>
-        <Stack gap={2} className="align-items-center justify-content-center h-100">
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
           <span className="fs-5">{title}</span>
           {tags.length > 0 && (
-            <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap"> 
-            {tags.map(tag =>(
-              <Badge key={tag.id} className="text-truncate">{tag.label}</Badge>
-            ))}
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
+              ))}
             </Stack>
           )}
         </Stack>
